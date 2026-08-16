f55cbd9683886f2b67d185f7b4ac2b208a61019b
dave_tu <a0987293061@gmail.com>
2026-08-16 23:16:40 +0800

    加入根目錄跳轉頁面

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>頁面跳轉中...</title>
+    <script>
+        window.location.href = '/public';
+    </script>
+</head>
+<body>
+    <p>頁面跳轉中，請稍候...</p>
+</body>
+</html>
